0e5ab4041b7e94ed92f8453584b01cfb1086101f
IALTAIR <yferoze@gmail.com>
2024-10-25 15:39:21 +0200

    modif product.js

--- back/routes/product.js
+++ back/routes/product.js
@@ -8,7 +8,3 @@ router.get("/:id", productCtrl.getOneProduct);
 router.post("/order", productCtrl.orderProducts);
 
 module.exports = router;
-
-module.exports = (req, res) => {
-  res.status(200).json({ message: "Hello from the product route!" });
-};
