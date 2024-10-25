3547accb261c7722eb807d9a3af510f38d2fb8c3
IALTAIR <yferoze@gmail.com>
2024-10-25 15:26:52 +0200

    modifcation vercel.json et product.js en backend

--- back/routes/product.js
+++ back/routes/product.js
@@ -1,10 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const productCtrl = require('../controllers/product');
+const productCtrl = require("../controllers/product");
 
-router.get('/', productCtrl.getAllProducts);
-router.get('/:id', productCtrl.getOneProduct);
-router.post('/order', productCtrl.orderProducts);
+router.get("/", productCtrl.getAllProducts);
+router.get("/:id", productCtrl.getOneProduct);
+router.post("/order", productCtrl.orderProducts);
 
 module.exports = router;
+
+module.exports = (req, res) => {
+  res.status(200).json({ message: "Hello from the product route!" });
+};
