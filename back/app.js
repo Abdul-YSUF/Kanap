383d43e6e59b55a9e6c636c267f19ceab6c4b212
IALTAIR <yferoze@gmail.com>
2024-10-25 16:21:27 +0200

    modif app js

--- back/app.js
+++ back/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
-
 const productRoutes = require('./routes/product');
 
 const app = express();
+const port = 3000;  // Assurez-vous que le port est bien défini
 
+// Middleware CORS
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -12,12 +13,20 @@ app.use((req, res, next) => {
   next();
 });
 
+// Middleware pour les fichiers statiques
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(express.static('images'));
 
-app.use(express.urlencoded({extended: true}));
+// Middleware pour parser les données
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Routes
 app.use('/api/products', productRoutes);
+
+// Démarrer le serveur
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
 
 module.exports = app;
